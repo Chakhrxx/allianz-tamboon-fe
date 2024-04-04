1027b90284182b9cb90848d1eac4eecfaa9000a0
“Chxkhrx” <chakhree.h@gmail.com>
2024-04-05 02:28:11 +0700

    minimize ui

--- src/components/GameIcon.tsx
+++ src/components/GameIcon.tsx
@@ -1,40 +1,40 @@
-import { Game } from '@/constants/game'
+import { Game } from "@/constants/game";
 
-import SwimmingIcon from '@/assets/svgs/swimming.svg?react'
-import AthleticIcon from '@/assets/svgs/athletic.svg?react'
-import BasketballIcon from '@/assets/svgs/basketball.svg?react'
-import HulaHoopIcon from '@/assets/svgs/hulahoop.svg?react'
-import TugOfWarIcon from '@/assets/svgs/tugofwar.svg?react'
-import CyclingIcon from '@/assets/svgs/cycling.svg?react'
-import BoxingIcon from '@/assets/svgs/boxing.svg?react'
-import RunningIcon from '@/assets/svgs/running.svg?react'
-import { useMemo } from 'react'
+import SwimmingIcon from "@/assets/svgs/swimming.svg?react";
+import AthleticIcon from "@/assets/svgs/athletic.svg?react";
+import BasketballIcon from "@/assets/svgs/basketball.svg?react";
+import HulaHoopIcon from "@/assets/svgs/hulahoop.svg?react";
+import TugOfWarIcon from "@/assets/svgs/tugofwar.svg?react";
+import CyclingIcon from "@/assets/svgs/cycling.svg?react";
+import BoxingIcon from "@/assets/svgs/boxing.svg?react";
+import RunningIcon from "@/assets/svgs/running.svg?react";
+import { useMemo } from "react";
 
 type Props = React.SVGProps<SVGSVGElement> & {
-  game: Game
-}
+  game: Game;
+};
 
 export default function GameIcon({ game, ...props }: Props) {
   const Icon = useMemo(() => {
     switch (game) {
       case Game.Swimming:
-        return SwimmingIcon
+        return SwimmingIcon;
       case Game.Hurdle:
-        return AthleticIcon
+        return AthleticIcon;
       case Game.Basketball:
-        return BasketballIcon
+        return BasketballIcon;
       case Game.HulaHoop:
-        return HulaHoopIcon
+        return HulaHoopIcon;
       case Game.TugOfWar:
-        return TugOfWarIcon
+        return TugOfWarIcon;
       case Game.Cycling:
-        return CyclingIcon
+        return CyclingIcon;
       case Game.Boxing:
-        return BoxingIcon
+        return BoxingIcon;
       case Game.Running:
-        return RunningIcon
+        return RunningIcon;
     }
-  }, [game])
+  }, [game]);
 
-  return <Icon {...props} />
+  return <Icon {...props} />;
 }
