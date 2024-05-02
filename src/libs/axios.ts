e0ec9e7ea84b5acba1a48e72e17c762d3b3ce67e
“Chxkhrx” <chakhree.h@gmail.com>
2024-05-02 22:38:03 +0700

    api

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,13 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-})
+  baseURL: import.meta.env.VITE_API_URL ?? "http://159.223.44.23:3000",
+});
 
 axiosInstance.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem('token')
+  const accessToken = localStorage.getItem("token");
   if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`
+    config.headers.Authorization = `Bearer ${accessToken}`;
   }
-  return config
-})
+  return config;
+});
