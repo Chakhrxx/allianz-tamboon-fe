c090a64997b2f699abe3f511b8edf059dda32af3
“Chxkhrx” <chakhree.h@gmail.com>
2024-05-23 22:16:39 +0700

    update page title

--- src/pages/(protected)/tamboon/index.tsx
+++ src/pages/(protected)/tamboon/index.tsx
@@ -5,7 +5,6 @@ function CSRPage() {
   return (
     <>
       <div className="relative z-10 text-center p-6 bg-white rounded-t-[38px]">
-        <div className="bg-[#D9D9D9] w-40 h-2 rounded-full mx-auto"></div>
         <Actives />
         <Histories />
       </div>
