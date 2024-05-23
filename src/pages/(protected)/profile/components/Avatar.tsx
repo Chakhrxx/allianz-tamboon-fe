04e01b8380f4510dcb27744dbcd58d7c4b27b83d
“Chxkhrx” <chakhree.h@gmail.com>
2024-05-23 21:58:15 +0700

    check drop shadow

--- src/pages/(protected)/profile/components/Avatar.tsx
+++ src/pages/(protected)/profile/components/Avatar.tsx
@@ -36,7 +36,7 @@ function Avatar({ url, containerClassName, onFileUpload }: Props) {
       />
 
       {url ? (
-        <div className="w-20 h-20 border-4 border-white rounded-full overflow-hidden  drop-shadow-2xl ">
+        <div className="w-20 h-20 border-4 border-white rounded-full overflow-hidden">
           <img
             className="w-full h-full object-cover"
             src={url}
